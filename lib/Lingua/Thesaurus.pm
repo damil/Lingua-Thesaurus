062ed16ea30dc4f701b9f00edbdd8eb27101e1c5
Laurent Dami <dami@cpan.org>
2013-06-05 04:08:46 +0200

    bump version (0.07)

--- lib/Lingua/Thesaurus.pm
+++ lib/Lingua/Thesaurus.pm
@@ -5,7 +5,7 @@ use Module::Load ();
 use Carp;
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 has 'storage'          => (is => 'ro', does => 'Lingua::Thesaurus::Storage',
                            handles => [qw/search_terms fetch_term
